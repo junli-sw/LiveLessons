927e047ed8ccdcccceb7374ecf50326e197c6243
junli-sw <jun.r.li@gmail.com>
2025-10-24 13:03:51 -0400

    Add Spring Boot and AspectJ support

--- Application.java
+++ Application.java
@@ -1,3 +1,7 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 @EnableAspectJAutoProxy
 @SpringBootApplication(scanBasePackages = "com.yourcompany")
 public class Application {
